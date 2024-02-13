ee1fd4ff4ddfb7b0c569478bbc77b26752b2861c
Antonio Tomita <atomita@uolinc.com>
2024-02-13 20:37:23 -0300

    first commit

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,46 @@
+import Botao from "../Botao";
+import Relogio from "./Relogio";
+import style from './Cronometro.module.scss';
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
+
+interface Props {
+    selecionado: ITarefa | undefined,
+    finalzarTarefa: () => void
+}
+
+export default function Cronometro({ selecionado, finalzarTarefa } :  Props) {
+
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect(() => {
+        if(selecionado?.tempo){
+            setTempo(tempoParaSegundos(selecionado.tempo));
+        }       
+    }, [selecionado]);
+
+    function regressiva(contador: number = 0) {
+        setTimeout(() => {
+            if(contador > 0) {
+                setTempo(contador - 1);
+                return regressiva(contador - 1);
+            }
+            finalzarTarefa();
+        }, 1000);
+    }
+
+    return (
+        <div className={style.cronometro}>
+            <p className={style.titulo}>Estolha um card e inicie o cronômetro</p>
+            <div className={style.relogioWrapper}>
+                <Relogio 
+                    tempo={tempo}
+                />
+            </div>
+            <Botao onClick={() => regressiva(tempo)}>
+                Começar
+            </Botao>
+        </div>
+    )
+}
